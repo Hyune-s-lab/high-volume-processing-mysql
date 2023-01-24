6990cd981512bb41600b6fcd4168a5977ae3267d
Hyune-c <choi8608@gmail.com>
2023-01-24 16:20:05 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "high-volume-processing-mysql"
---
+++ src/main/kotlin/com/example/highvolumeprocessingmysql/HighVolumeProcessingMysqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.highvolumeprocessingmysql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HighVolumeProcessingMysqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<HighVolumeProcessingMysqlApplication>(*args)
+}
