1990990839499c25aa91da9bc71b76a51c28c7e4
Hyune-c <choi8608@gmail.com>
2023-01-24 21:12:24 +0900

    [04 SNS 모델링으로 배우는 정규화/비정규화] 회원정보 등록 구현 - jpa auditing 연동

--- src/main/kotlin/com/example/highvolumeprocessingmysql/domain/member/entity/Member.kt
+++ src/main/kotlin/com/example/highvolumeprocessingmysql/domain/member/entity/Member.kt
@@ -2,17 +2,10 @@ package com.example.highvolumeprocessingmysql.domain.member.entity
 
 import java.time.LocalDate
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
 
 @Entity
 class Member(
     val nickname: String,
     val email: String,
     val birthday: LocalDate
-) {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-}
+) : BaseEntity()
